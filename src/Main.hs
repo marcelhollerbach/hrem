c1ec4b11413eed91b56b056e80c3abbc803af096
Marcel Hollerbach <marcel-hollerbach@t-online.de>
2015-12-06 20:18:37 +0100

    lvl up main!lvl up main!!!

--- src/Main.hs
+++ src/Main.hs
@@ -2,34 +2,40 @@ module Main where
 import Circle
 import Point
 import Rectangle
-import Renderer
-import SoftwareRenderer
+import Shape
+import Renderer as R
+import qualified SoftwareRenderer as S
 import Data.Colour
 import Data.Colour.SRGB
 import Data.Colour.Names
 import Data.Matrix
 import Codec.Picture
 
-frame :: SoftwareBuffer;
+-- a clean buffer
+frame :: S.SoftwareBuffer;
 frame = emptyFrame 1000 1000
 
+simpleTransform :: Point -> Point
+simpleTransform (Point x y) = pointCreate (x * 2) y
+
+-- debugging list with a bunch of primitives
 debuglist :: [Primitiv]
+debuglist = (map noTransformShape [R.Rect (rectCreate2  0  0 200 200 (withOpacity black 100)),
+                                   R.Rect (rectCreate2 200 200 200 200 (opaque red)),
+                                   R.Rect (rectCreate2 400 400 200 200 (opaque yellow))]) ++
+            [Primitiv simpleTransform (R.Circle (circleCreate2 400 400 200 (withOpacity blue 50)))]
 
-debuglist = [Rect (rectCreate2  0  0 200 200 (opaque black)),
-             Rect (rectCreate2 200 200 200 200 (opaque red)),
-             Rect (rectCreate2 400 400 200 200 (opaque green)),
-             Circle (circleCreate2 400 400 200 (opaque blue))]
-
-maxn :: Double
-maxn = 255
-
-c :: Double -> Pixel8
-c d = fromInteger $ round d
+-- converts a Double to a pixel8 vaue
+convert :: Double -> Pixel8
+convert d = fromInteger $ round d
 
 -- converts SRGB with values between 0 and 1 to PixelRGB 8 with values between 0 and 256
 pixelConvert :: RGB Double -> PixelRGB8
-pixelConvert (RGB r g b)= PixelRGB8 (c (r* maxn)) (c (g* maxn)) (c (b* maxn))
+pixelConvert (RGB r g b)= PixelRGB8 (convert (r* 255))
+                                    (convert (g* 255))
+                                    (convert (b* 255))
 
-outputMatrix = swBufferMatrix $ renderFramePrimitives frame debuglist
+outputMatrix = S.swBufferMatrix $ renderFramePrimitives frame debuglist
+
 main = writePng "/tmp/lucker.png" $ generateImage converter 1000 1000 where
-    converter x y = pixelConvert $ toSRGB $ getElem (x+1) (y+1) outputMatrix
+   converter x y = pixelConvert $ toSRGB $ getElem (x+1) (y+1) outputMatrix
